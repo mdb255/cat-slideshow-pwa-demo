05963e1dc2ec79f96a33c9a33e4430cb87c39897
Mike B <mdb255.dev@gmail.com>
2025-10-23 13:57:20 -0400

    Add /cat_images endpoint for listing all S3 bucket cat images

--- back-end/cat-slideshow-api/src/app/api/__init__.py
+++ back-end/cat-slideshow-api/src/app/api/__init__.py
@@ -2,9 +2,11 @@
 
 from .cats import router as cats_router
 from .slideshows import router as slideshows_router
+from .cat_images import router as cat_images_router
 
 __all__ = [
     "cats_router", 
     "slideshows_router",
+    "cat_images_router",
 ]
 
--- back-end/cat-slideshow-api/src/app/settings.py
+++ back-end/cat-slideshow-api/src/app/settings.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
+    cat_images_aws_access_key_id: str
+    cat_images_aws_secret_access_key: str
+    cat_images_bucket_name: str = "cat-slideshow-demo"
 
     model_config = SettingsConfigDict(
         env_file=".env",
