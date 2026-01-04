3199ff7f269f5c6f7736e4bd4de237ebc0bb6e9e
Mike B <mdb255.dev@gmail.com>
2026-01-04 17:35:59 -0500

    Update Favicon bg color and PWA name

--- front-end/cat-slideshow/vite.config.ts
+++ front-end/cat-slideshow/vite.config.ts
@@ -4,33 +4,33 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+    plugins: [react(), VitePWA({
+        registerType: 'prompt',
+        injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+        pwaAssets: {
+            disabled: false,
+            config: true,
+        },
 
-    manifest: {
-      name: 'cat-slideshow',
-      short_name: 'cat-slideshow',
-      description: 'cat-slideshow',
-      theme_color: '#6d0fab',
-    },
+        manifest: {
+            name: 'Cat Slideshow Demo',
+            short_name: 'Cat Slideshow',
+            description: 'Cat Slideshow Demo',
+            theme_color: '#6d0fab',
+        },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+        workbox: {
+            globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+            cleanupOutdatedCaches: true,
+            clientsClaim: true,
+        },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+        devOptions: {
+            enabled: false,
+            navigateFallback: 'index.html',
+            suppressWarnings: true,
+            type: 'module',
+        },
+    })],
 })
