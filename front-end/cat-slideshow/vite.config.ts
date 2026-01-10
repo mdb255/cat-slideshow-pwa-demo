d49ffd06e61417a0c21d5f32dcd73befc8ab78ef
Mike B <mdb255.dev@gmail.com>
2026-01-09 22:47:57 -0500

    Replace favicon with new version and update PWA configuration to include additional icons

--- front-end/cat-slideshow/vite.config.ts
+++ front-end/cat-slideshow/vite.config.ts
@@ -18,6 +18,22 @@ export default defineConfig({
             short_name: 'Cat Slideshow',
             description: 'Cat Slideshow Demo',
             theme_color: '#6d0fab',
+            background_color: '#6d0fab',
+            icons: [
+                {
+                    src: 'favicon_v2.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                    purpose: 'any'
+                },
+                {
+                    // Used when installing on Android
+                    src: 'favicon_maskable.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable'
+                }
+            ]
         },
 
         workbox: {
